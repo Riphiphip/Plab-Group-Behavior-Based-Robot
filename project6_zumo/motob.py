fb13f52498503c37563acae4664e62ec0e134b3a
Andreas K. Berg <andreaskb98@gmail.com>
2019-10-24 14:24:34 +0200

    Update Motob

--- project6_zumo/motob.py
+++ project6_zumo/motob.py
@@ -19,7 +19,7 @@ class Motob:
     each wheel
     """
 
-    def __init__(self, motors=[]):
+    def __init__(self, timestep, motors=[], turn_speed=1):
         """It contains (at least) the following instance variables:
             1. motors - a list of the motors whose settings will be determined by the motob.
             2. value - a holder of the most recent motor recommendation sent to the motob.
@@ -28,26 +28,37 @@ class Motob:
             operationalize it.
             2. operationalize - convert a motor recommendation into one or more motor settings,
             which are sent to the corresponding motor(s)."""
+        self.duration = timestep
+        self.turn_speed = turn_speed
         self.motors = motors
         self.value = None
 
-    def update(self, motor_recommendation):
+    def update(self, motor_recommendation: (int, float)):
         """Set motors to recommended settings"""
         self.value = motor_recommendation
         self.operationalize(motor_recommendation)
 
-    def operationalize(self, motor_recommendation):
+    def operationalize(self, motor_recommendation: (int, float)):
+        """Convert a motor recommendation into one or more motor settings,
+            which are sent to the corresponding motor(s)."""
         settings, duration = convert_recommendation_to_motor_settings(
             motor_recommendation)
         for i in range(len(self.motors)):
             self.motors[i].set_value(settings[i], dur=duration)
 
-    @staticmethod
-    def convert_recommendation_to_motor_settings(motor_recommendation):
+    def convert_recommendation_to_motor_settings(self, motor_recommendation: (int, float)):
+        """
+        Convert MR to MS.
+        motor_recommendation:
+         - [0]: int in [0,2], no rotation, right or left
+         - [1]: float speed, negative for bakwards
+        """
         direction = motor_recommendation[0]
-        angle = motor_recommendation[1]
+        speed = motor_recommendation[1]
 
-        # Må teste hvordan man skal få ut vinkel med motorspeeds og duration, Lineært forhold?
-        motor_settings = None
-        duration = None
-        return motor_settings, duration
+        if direction == 0:
+            return (speed, speed), self.duration
+        elif direction == 1:
+            return (speed + self.turn_speed, speed - self.turn_speed), self.duration
+        elif direction == 2:
+            return (speed - self.turn_speed, speed + self.turn_speed), self.duration
