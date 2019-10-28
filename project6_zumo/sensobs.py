a626f3cfd080c55cf605fed22d96994028636231
riphiphip <halvor.bjorstad@gmail.com>
2019-10-28 11:18:17 +0100

    Moved sensobs to one file

--- project6_zumo/sensobs.py
+++ project6_zumo/sensobs.py
@@ -4,6 +4,10 @@ Created on Thu Oct 24 08:33:57 2019
 
 @author: Joule
 """
+
+import numpy as np
+import cv2 as cv
+
 from abc import ABC, abstractmethod
 
 class Sensob(ABC):
@@ -36,3 +40,32 @@ for many additional forms of preprocessing."""
     @abstractmethod
     def preprocess(self, sensor_data):
         '''Preprocessing of raw data'''
+
+
+class FaceFinder(Sensob):
+
+    def __init__(self, sensors=[]):
+        super().__init__(sensors=sensors)
+        file_path = "resources/haarcascade_frontalface_default.xml"
+        self.cascade = cv.CascadeClassifier(file_path)
+
+    def update(self):
+        for camera in self.sensors:
+            camera.update()
+        raw_output = [s.get_value() for s in self.sensors]
+        self.prevData = self.preprocess(raw_output)
+
+    def get_value(self):
+        return self.prevData
+
+    def preprocess(self, sensor_data):
+        output = []
+        for image in sensor_data:
+            current_camera = []
+            cv_image = np.array(image)
+            cv_image = cv.cvtColor(cv_image, cv.COLOR_RGB2GRAY)
+            faces = self.cascade.detectMultiScale(cv_image, 1.1, 4)
+            for (x, y, w, h) in faces:
+                current_camera.append([x,y,w,h])
+            output.append(current_camera)
+        return output
