46fdc7cd03dc04f80045ce63186d0d8e5b50ca87
Andreas K. Berg <andreaskb98@gmail.com>
2019-10-28 15:35:54 +0100

    Update main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from project6_zumo.bbcon import BBCON
 from project6_zumo.arbitrator import Arbitrator
 from project6_zumo.motob import Motob
-from project6_zumo.behaviors import RemoteControl
+from project6_zumo.behaviors import RemoteControl, EdgeDetection
 
 import sys
 
@@ -12,8 +12,10 @@ def main():
     a = Arbitrator()
     controller = BBCON(a)
     print("Created controller")
-    controller.add_behavior(RemoteControl(controller, 10, [sys.stdin]))
+    controller.add_behavior(RemoteControl(10))
+    controller.add_behavior(EdgeDetection(100))
     controller.activate_behavior(controller.behaviors[0])
+    controller.activate_behavior(controller.behaviors[1])
     print("Added behaviors:")
     for c in a.behaviors:
         print(c)
