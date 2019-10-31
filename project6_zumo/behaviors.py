a429fc860cf3db89af43182e682c61822f9239a4
joulebit <45593399+joulebit@users.noreply.github.com>
2019-10-31 10:16:31 +0100

    glemte å returne motor recomendation

--- project6_zumo/behaviors.py
+++ project6_zumo/behaviors.py
@@ -121,6 +121,7 @@ class EdgeDetection(Behavior):
     def sense_and_act(self):
         """the core computations performed by the behavior that use sensob readings
         to produce motor recommendations (and halt requests)"""
+        return self.motor_recommendation
     
     def __str__(self):
         return "EdgeDetection Behavior"
