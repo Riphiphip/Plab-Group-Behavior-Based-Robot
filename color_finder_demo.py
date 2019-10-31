dee7c943fa2885ed35d6351ea0241f91f2c1e405
riphiphip <halvor.bjorstad@gmail.com>
2019-10-31 14:28:39 +0100

    Added test-module for ColorFinder

---
+++ color_finder_demo.py
@@ -0,0 +1,13 @@
+from project6_zumo.sensobs import ColorFinder
+from project6_supply.sensors.camera import Camera
+
+def main(c: ColorFinder):
+    print(c.get_value())
+
+if __name__ == "__main__":
+    c = ColorFinder(sensors=[Camera()])
+    try:
+        while True:
+            main(c)
+    except KeyboardInterrupt:
+        pass
