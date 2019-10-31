b716b305d9b71be13654aba892dc44d6601e7409
riphiphip <halvor.bjorstad@gmail.com>
2019-10-31 14:55:26 +0100

    Fix:Sensors don't update

--- color_finder_demo.py
+++ color_finder_demo.py
@@ -3,11 +3,14 @@ from time import sleep
 from project6_zumo.sensobs import ColorFinder
 from project6_supply.sensors.camera import Camera
 
-def main(c: ColorFinder):
+def main(c: ColorFinder, cam: Camera):
+    cam.update()
+    c.update()
     print(c.get_value())
 
 if __name__ == "__main__":
-    c = ColorFinder(sensors=[Camera()])
+    cam = Camera()
+    c = ColorFinder(sensors=[c])
     try:
         while True:
             main(c)
