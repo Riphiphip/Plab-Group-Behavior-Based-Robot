740744cc2828c1c4af7e67d279838cdff12b7cee
riphiphip <halvor.bjorstad@gmail.com>
2019-10-31 16:09:14 +0100

    Revert "Made camera take pictures faster"
    This reverts commit ec8d40891eb73ba1a0b070dd0b9da9ebd6b7c8a3.
    
    Shit's fucked yo

--- project6_supply/sensors/camera.py
+++ project6_supply/sensors/camera.py
@@ -31,7 +31,7 @@ class Camera():
     def sensor_get_value(self):
         # This is a OS call that takes a image and makes it accessible to PIL
         # operations in the same directory
-        os.system('raspistill -t 0.002 -o project6_supply/sensors/image.' + self.filetype + ' -w "' + str(self.img_width) +
+        os.system('raspistill -t 1 -o project6_supply/sensors/image.' + self.filetype + ' -w "' + str(self.img_width) +
                   '" -h "' + str(self.img_height) + '" -rot "' + str(self.img_rot) + '"')
         # Open the image just taken by raspicam
         # Stores the RGB array in the value field
