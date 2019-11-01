c5db4defa810dfdb5054c05385c00406f8f91b99
Riphiphip <halvor.bjorstad@gmail.com>
2019-11-01 20:32:44 +0100

    New debug code

--- color_finder_demo.py
+++ color_finder_demo.py
@@ -2,18 +2,21 @@ from time import sleep
 
 from project6_zumo.sensobs import ColorFinder
 from project6_supply.sensors.camera import Camera
+from project6_zumo.behaviors import ColorChasing
 
-def main(c: ColorFinder, cam: Camera):
+def main(c: ColorFinder, cam: Camera, b: ColorChasing):
     cam.update()
     c.update()
-    print(c.get_value())
+    b.update()
+    print(c.get_value(), " --> ", b.motor_recommendation, b.get_weight())
 
 if __name__ == "__main__":
     cam = Camera()
     c = ColorFinder(sensors=[cam])
+    b = ColorChasing(10, sensors=[c])
     try:
         while True:
-            main(c, cam)
+            main(c, cam, b)
             sleep(0.5)
     except KeyboardInterrupt:
         pass
