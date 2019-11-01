5896f2470a2d25e228a7e99dddbaacce6c2a5b20
Riphiphip <halvor.bjorstad@gmail.com>
2019-11-01 20:34:45 +0100

    bugfix

--- color_finder_demo.py
+++ color_finder_demo.py
@@ -1,6 +1,6 @@
 from time import sleep
 
-from project6_zumo.sensobs import ColorFinder
+from project6_zumo.sensobs import ColorFinder, Collition
 from project6_supply.sensors.camera import Camera
 from project6_zumo.behaviors import ColorChasing
 
@@ -13,7 +13,7 @@ def main(c: ColorFinder, cam: Camera, b: ColorChasing):
 if __name__ == "__main__":
     cam = Camera()
     c = ColorFinder(sensors=[cam])
-    b = ColorChasing(10, sensors=[c])
+    b = ColorChasing(10, sensors=[c, Collition()])
     try:
         while True:
             main(c, cam, b)
